1315b96e2a49001ff3e75bd2ecac9a952bf36c8d
Johed Velca <jv13415@gmail.com>
2022-12-06 17:49:11 -0500

    working until SongDetailsPage

---
+++ lyriks/src/redux/services/shazamCore.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "effbfcaeb6mshb2dffd477419163p1e967fjsn4bf523bc8647"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
