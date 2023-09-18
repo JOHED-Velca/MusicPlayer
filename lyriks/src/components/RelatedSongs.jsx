1dbe143dfe86dd7d5e1f7b7eb81a793ab458d134
Johed Velca <jv13415@gmail.com>
2023-09-17 20:25:11 -0500

    until 2:18:15

--- lyriks/src/components/RelatedSongs.jsx
+++ lyriks/src/components/RelatedSongs.jsx
@@ -1,5 +1,31 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseCLick,
+  handlePlayClick,
+  artistId,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+
+    <div className="mt-5 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseCLick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
